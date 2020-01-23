2956d988093df8b163ad4b147079a562b919267f
Mestpal <manuestpal@gmail.com>
2020-01-23 19:07:07 +0100

    Index page has a custom canvas component

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,17 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      INDEX
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <customCanvas :circleDiameter="200" />
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-export default {
+import customCanvas from '../components/customCanvas'
 
+export default {
+  components: {
+    customCanvas
+  }
 }
 </script>
