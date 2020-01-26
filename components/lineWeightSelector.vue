096b4f6a531f20656e241592bf04d454e1847f24
Mestpal <manuestpal@gmail.com>
2020-01-26 14:55:08 +0100

    Line weight selector component

---
+++ components/lineWeightSelector.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-card>
+    <v-card-title> Line Weight </v-card-title>
+    <v-card-text>
+      <lineOption
+        v-for="(lineOption, index) in lineWeightsOptions"
+        v-bind:key="index"
+        :weight="lineOption"
+        @lineEvent="updateLineWeight($event)"
+      />
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import lineWeightActions from '@/mixins/lineWeightActions'
+import lineOption from '@/components/atomics/lineOption'
+
+export default {
+  components: {
+    lineOption
+  },
+  mixins: [lineWeightActions],
+  methods: {
+    updateLineWeight (lineEvent) {
+      console.log(`Update the line weight to ${lineEvent.weight}`)
+      this.updateLineWeightAction(lineEvent)
+    }
+  }
+}
+</script>
