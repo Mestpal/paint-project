1cac13a1d550fe575261e39295efefef7fb0d889
Mestpal <manuestpal@gmail.com>
2020-01-26 14:54:24 +0100

    line opton atomic component

---
+++ components/atomics/lineOption.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    @click="lineWeightOptionSelected"
+  >
+    LINE {{ lineConf.weight }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    weight: {
+      type: Number,
+      default: 0
+    }
+  },
+  data () {
+    return {
+      lineConf: {
+        weight: this.weight
+      }
+    }
+  },
+  methods: {
+    lineWeightOptionSelected () {
+      console.log('Line weight selected')
+      this.$emit('lineEvent', this.lineConf)
+    }
+  }
+}
+</script>
