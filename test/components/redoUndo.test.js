76ba39885601342665ae185beb023ca83422f035
Mestpal <manuestpal@gmail.com>
2020-01-28 22:44:53 +0100

    add methods tests for redoUndo component

--- test/components/redoUndo.test.js
+++ test/components/redoUndo.test.js
@@ -5,27 +5,50 @@ import Vuex from 'vuex'
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 
 import redoUndo from '@/components/redoUndone'
-import { getters } from '@/mixins/drawModules/redoUndoActions'
+import redoUndoActions from '@/mixins/drawModules/redoUndoActions'
 
 Vue.use(Vuetify)
 Vue.use(Vuex)
 const localVue = createLocalVue()
 
 describe('redoUndo.vue', () => {
-  let store
   let wrapper
 
   beforeEach(() => {
-    store = new Vuex.Store({
-      getters
-    })
     wrapper = shallowMount(redoUndo, {
-      store,
+      mixins: [redoUndoActions],
       localVue
     })
   })
 
   it('redoUndo component renders correctly', () => {
     expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('selectButtonAction undo', () => {
+    wrapper.vm.undoLastDrawStep = jest.fn()
+      .mockImplementation(() => 1)
+
+    wrapper.vm.selectButtonAction('undo')
+    expect(wrapper.vm.undoLastDrawStep).toHaveBeenCalled()
+  })
+
+  it('selectButtonAction redo', () => {
+    wrapper.vm.redoDrawStepAction = jest.fn()
+      .mockImplementation(() => 1)
+
+    wrapper.vm.selectButtonAction('redo')
+    expect(wrapper.vm.redoDrawStepAction).toHaveBeenCalled()
+  })
+
+  it('selectButtonAction default', () => {
+    wrapper.vm.redoDrawStepAction = jest.fn()
+      .mockImplementation(() => 1)
+    wrapper.vm.undoLastDrawStep = jest.fn()
+      .mockImplementation(() => 1)
+
+    wrapper.vm.selectButtonAction('default')
+    expect(wrapper.vm.redoDrawStepAction).toHaveBeenCalledTimes(0)
+    expect(wrapper.vm.undoLastDrawStep).toHaveBeenCalledTimes(0)
   })
 })
